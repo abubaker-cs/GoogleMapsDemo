9ec59b0ee0f31218b5c87b5344b3ddc2edf5a35b
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-12-04 05:00:46 +0500

    Base setup

---
+++ README.md
@@ -0,0 +1,7 @@
+# Google Maps SDK
+
+Understanding Google's Map SDK for Android
+
+## Custom Styling Wizard
+
+https://mapstyle.withgoogle.com/ 
--- app/src/main/java/org/abubaker/googlemapsdemo/MapsActivity.kt
+++ app/src/main/java/org/abubaker/googlemapsdemo/MapsActivity.kt
@@ -1,6 +1,8 @@
 package org.abubaker.googlemapsdemo
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -29,6 +31,95 @@ class MapsActivity :
 
         //
         mapFragment.getMapAsync(this)
+    }
+
+    /**
+     * Initialize the contents of the Activity's standard options menu.  You
+     * should place your menu items in to <var>menu</var>.
+     *
+     *
+     * This is only called once, the first time the options menu is
+     * displayed.  To update the menu every time it is displayed, see
+     * [.onPrepareOptionsMenu].
+     *
+     *
+     * The default implementation populates the menu with standard system
+     * menu items.  These are placed in the [Menu.CATEGORY_SYSTEM] group so that
+     * they will be correctly ordered with application-defined menu items.
+     * Deriving classes should always call through to the base implementation.
+     *
+     *
+     * You can safely hold on to <var>menu</var> (and any items created
+     * from it), making modifications to it as desired, until the next
+     * time onCreateOptionsMenu() is called.
+     *
+     *
+     * When you add items to the menu, you can implement the Activity's
+     * [.onOptionsItemSelected] method to handle them there.
+     *
+     * @param menu The options menu in which you place your items.
+     *
+     * @return You must return true for the menu to be displayed;
+     * if you return false it will not be shown.
+     *
+     * @see .onPrepareOptionsMenu
+     *
+     * @see .onOptionsItemSelected
+     */
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // return super.onCreateOptionsMenu(menu)
+
+        menuInflater.inflate(R.menu.map_types, menu)
+        return true
+
+    }
+
+    /**
+     * This hook is called whenever an item in your options menu is selected.
+     * The default implementation simply returns false to have the normal
+     * processing happen (calling the item's Runnable or sending a message to
+     * its Handler as appropriate).  You can use this method for any items
+     * for which you would like to do processing without those other
+     * facilities.
+     *
+     *
+     * Derived classes should call through to the base class for it to
+     * perform the default menu handling.
+     *
+     * @param item The menu item that was selected.
+     *
+     * @return boolean Return false to allow normal menu processing to
+     * proceed, true to consume it here.
+     *
+     * @see .onCreateOptionsMenu
+     */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+
+            R.id.normal_map -> {
+                map.mapType = GoogleMap.MAP_TYPE_NORMAL
+            }
+
+            R.id.hybrid_map -> {
+                map.mapType = GoogleMap.MAP_TYPE_HYBRID
+            }
+
+            R.id.satellite_map -> {
+                map.mapType = GoogleMap.MAP_TYPE_SATELLITE
+            }
+
+            R.id.terrain_map -> {
+                map.mapType = GoogleMap.MAP_TYPE_TERRAIN
+            }
+
+            R.id.none_map -> {
+                map.mapType = GoogleMap.MAP_TYPE_NONE
+            }
+        }
+
+        return true
+
     }
 
     /**
